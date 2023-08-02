7f94929860201153ff238b715d10f6c7b2d3db7e
Mukund <86926283+mukund47@users.noreply.github.com>
2023-08-03 01:19:41 +0530

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,147 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            // Remove "active" class from all navigation links
+            navLink.forEach(link => link.classList.remove('active'));
+
+            // Add "active" class to the link that corresponds to the current section
+            const correspondingNavLink = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+            if (correspondingNavLink) {
+                correspondingNavLink.classList.add('active');
+            }
+        }
+    });
+}
+
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/*SCROLL HOME*/
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', {delay: 200})
+sr.reveal('.home__img', {origin:'right', delay: 400})
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img', {delay: 500})
+sr.reveal('.about__subtitle', {delay: 300})
+sr.reveal('.about__profession', {delay: 400})
+sr.reveal('.about__text', {delay: 500})
+sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
+sr.reveal('.skills__img', {delay: 400})
+
+/*SCROLL PORTFOLIO*/
+sr.reveal('.portfolio__img', {interval: 200})
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__input', {delay: 400})
+sr.reveal('.contact__button', {delay: 600})
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#78C1F3"
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
+
+const videoPlayer = document.getElementById('videoPlayer');
+const playPauseBtn = document.getElementById('playPauseBtn');
+const playPauseIcon = document.getElementById('playPauseIcon');
+const playPauseText = document.getElementById('playPauseText');
+
+function togglePlayPause() {
+    if (videoPlayer.paused || videoPlayer.ended) {
+        videoPlayer.play();
+        playPauseIcon.classList.remove('play-icon');
+        playPauseIcon.classList.add('pause-icon');
+        playPauseText.textContent = 'Pause';
+    } else {
+        videoPlayer.pause();
+        playPauseIcon.classList.remove('pause-icon');
+        playPauseIcon.classList.add('play-icon');
+        playPauseText.textContent = 'Play';
+    }
+}
+
+playPauseBtn.addEventListener('click', togglePlayPause);
